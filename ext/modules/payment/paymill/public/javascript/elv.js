facb690ed9f7777e4681b75d30b6ca7f1f2af61f
Harald Ponce de Leon <hpdl@oscommerce.com>
2013-07-18 15:59:16 +0200

    Add jQuery prop() compatibility function

--- ext/modules/payment/paymill/public/javascript/elv.js
+++ ext/modules/payment/paymill/public/javascript/elv.js
@@ -1,5 +1,15 @@
 var isElvSubmitted = false;
 $(document).ready(function () {
+    if (typeof $.fn.prop !== 'function') {
+        $.fn.prop = function(name, value) {
+            if (typeof value === 'undefined') {
+                return this.attr(name);
+            } else {
+                return this.attr(name, value);
+            }
+        };
+    }
+
     $('form[name="checkout_payment"]').submit(function () {
         if (!isElvSubmitted) {
             if ($("input[name=\'payment\'][value=\'paymill_elv\']").prop("checked")) {
