038a2e9f3b72e7d9a4f5cddd09d18db03cd70511
kazemek <>
2013-12-13 16:26:25 +0100

    POSC-7 add missing month lang constant

--- includes/languages/english/modules/payment/paymill_cc.php
+++ includes/languages/english/modules/payment/paymill_cc.php
@@ -33,6 +33,18 @@ define('MODULE_PAYMENT_PAYMILL_CC_TEXT_CREDITCARD_CARDNUMBER_INVALID','The credi
 define('MODULE_PAYMENT_PAYMILL_CC_TEXT_CREDITCARD_CVC_INVALID','The CVC field is a mandotory field.');
 define('MODULE_PAYMENT_PAYMILL_CC_TEXT_CREDITCARD_OWNER_INVALID','The Credit Card Owner field is invalid.');
 define('MODULE_PAYMENT_PAYMILL_CC_TEXT_CREDITCARD_CVC_TOOLTIP', 'What is a CVV/CVC number? Prospective credit cards will have a 3 to 4-digit number, usually on the back of the card. It ascertains that the payment is carried out by the credit card holder and the card account is legitimate. On Visa the CVV (Card Verification Value) appears after and to the right of your card number. Same goes for Mastercard’s CVC (Card Verfication Code), which also appears after and to the right of  your card number, and has 3-digits. Diners Club, Discover, and JCB credit and debit cards have a three-digit card security code which also appears after and to the right of your card number. The American Express CID (Card Identification Number) is a 4-digit number printed on the front of your card. It appears above and to the right of your card number. On Maestro the CVV appears after and to the right of your number. If you don’t have a CVV for your Maestro card you can use 000.');
+define('MODULE_PAYMENT_PAYMILL_CC_TEXT_MONTH_JANUARY','January');
+define('MODULE_PAYMENT_PAYMILL_CC_TEXT_MONTH_FEBRUARY','February');
+define('MODULE_PAYMENT_PAYMILL_CC_TEXT_MONTH_MARCH','March');
+define('MODULE_PAYMENT_PAYMILL_CC_TEXT_MONTH_APRIL','April');
+define('MODULE_PAYMENT_PAYMILL_CC_TEXT_MONTH_MAY','May');
+define('MODULE_PAYMENT_PAYMILL_CC_TEXT_MONTH_JUNE','June');
+define('MODULE_PAYMENT_PAYMILL_CC_TEXT_MONTH_JULY','July');
+define('MODULE_PAYMENT_PAYMILL_CC_TEXT_MONTH_AUGUST','August');
+define('MODULE_PAYMENT_PAYMILL_CC_TEXT_MONTH_SEPTEMBER','September');
+define('MODULE_PAYMENT_PAYMILL_CC_TEXT_MONTH_OCTOBER','October');
+define('MODULE_PAYMENT_PAYMILL_CC_TEXT_MONTH_NOVEMBER','November');
+define('MODULE_PAYMENT_PAYMILL_CC_TEXT_MONTH_DECEMBER','December');
 define('PAYMILL_0', 'General error in checkout.');
 define('PAYMILL_10001', 'General undefined response.');
 define('PAYMILL_10002', 'Still waiting on something.');
